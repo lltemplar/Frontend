7c9fca3cc7fb9bd3a31c0ab870551c9ee4c58af6
lltemplar <liaglin@163.com>
2014-09-14 15:07:37 +0800

    add Files

--- jQueryPlugin/js/jquery.pic.js
+++ jQueryPlugin/js/jquery.pic.js
@@ -2,7 +2,7 @@
  * jquery.pic.js 0.1
  * Copyright (c) 2014 lianglin
  * Date: 2014-08-17
- * lazyload:图片延时加载；picZoom:图片放大功能
+ * lazyload:图片延时加载；picZoom:图片放大功能;picSlide:图片幻灯片
  */
 (function($){
     $.fn.lazyLoad = function(options){
@@ -61,6 +61,75 @@
                 oImg.css("top",-percentY*(oImg.height() - oImgParent.height()) + "px");
             });
         });
+    };
+
+    $.fn.picSlide = function(options) {
+        var defaults = {autoPlay:true,autoPlaySpeed:3000,speed:500};
+        options = $.extend(defaults, options);
+        var imgList = $(this).find("ul");
+        var num = imgList.children().length;
+        var width = imgList.find("li").width();
+        imgList.css("width",width*(num+1));
+        imgList.append($("ul").children(0).clone());
+        var curPage = 0;
+        $(this).find(".navRight").click(function(){
+            if(imgList.is(":animated")){
+                return;
+            }
+            if(curPage === num){
+                curPage = 0;
+                imgList.css("left","0px");
+            }
+            imgList.animate({left:"-="+width},options.speed,function(){
+                curPage++;
+                setNav(curPage);
+            });
+        });
+        $(this).find(".navLeft").click(function(){
+            if(imgList.is(":animated")){
+                return;
+            }
+            if(curPage === 0){
+                curPage = num;
+                imgList.css("left",-1*width*num);
+            }
+            imgList.animate({left:"+="+width},options.speed,function(){
+                curPage--;
+                setNav(curPage);
+            });
+        });
+        var navBottom =  $(this).find(".navBottom");
+        navBottom.children().click(function(){
+            curPage = navBottom.children().index($(this));
+            setNav(curPage);
+            setPage(curPage);
+        });
+        function setNav(_curPage){
+            navBottom.children(".on").removeClass("on");
+            navBottom.children().eq(_curPage%num).addClass("on");
+            resetTimer();
+        }
+        function setPage(_curPage){
+            imgList.animate({left:-1*width*_curPage},options.speed);
+        }
+        if(options.autoPlay){
+            var handler = setInterval(autoPlay,options.autoPlaySpeed);
+        }
+        function autoPlay(){
+            if(curPage === num){
+                curPage = 0;
+                imgList.css("left","0px");
+            }
+            curPage++;
+            setPage(curPage);
+            setNav(curPage);
+        }
+        function resetTimer(){
+            if(!options.autoPlay)
+                return;
+            clearInterval(handler);
+            handler = setInterval(autoPlay,options.autoPlaySpeed);
+        }
     };
 })(jQuery);
 
